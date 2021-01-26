c140036fbcf40b2c949f031e77c2ea62fec0ca27
1BarCode <69029687+1BarCode@users.noreply.github.com>
2021-01-25 22:46:22 -0800

    switch case statement
    switch case: once a match has been made, no more matches will be run, all codes following will be executed until end of block or break is encountered

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-let js = "amazing";
-if (js === 'amazing') alert("JS is FUN!")
+const day = 'wednesday';
 
+switch(day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare videos');
+        break;
+    case 'wednesday':
+        console.log('Exercise')
+        // break;
+    case 'thursday':
+        console.log('Code');
+        // break;
+    case 'friday':
+        console.log('CodeRecord'); // this gets run since no more matching will be made
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day')            
+           
+}
