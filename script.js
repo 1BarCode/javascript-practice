c5f16fa08b21a15eaebefcf23a05868ec04e8428
1BarCode <69029687+1BarCode@users.noreply.github.com>
2021-01-25 22:56:46 -0800

    switch case vs if else

--- script.js
+++ script.js
@@ -22,6 +22,22 @@ switch(day) {
         console.log('Enjoy the weekend');
         break;
     default:
-        console.log('Not a valid day')            
-           
+        console.log('Not a valid day')                    
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare videos');
+} else if (day === 'wednesday') {
+    console.log('Exercise')
+} else if (day === 'thursday') {
+    console.log('Code')
+} else if (day === 'friday') {
+    console.log('CodeRecord')
+} else if (day === 'satuday' || day === 'sunday') {
+    console.log('Enjoy the weekend')
+} else {
+    console.log('Not a valid day')
 }
